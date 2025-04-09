329ebe460057199e1fdba9c89e252c3e86ff94e9
Nikita Korotov <korn03@vse.cz>
2025-04-09 04:11:57 +0200

    Cleanup

--- App.tsx
+++ App.tsx
@@ -71,54 +71,6 @@ Notifications.setNotificationHandler({
     }),
 });
 
-// function handleRegistrationError(errorMessage: string) {
-//     //alert(errorMessage);
-//     console.error(errorMessage);
-//     //throw new Error(errorMessage);
-// }
-//
-// async function registerForPushNotificationsAsync() {
-//     if (Platform.OS === 'android') {
-//         Notifications.setNotificationChannelAsync('default', {
-//             name: 'default',
-//             importance: Notifications.AndroidImportance.MAX,
-//             vibrationPattern: [0, 250, 250, 250],
-//             lightColor: '#FF231F7C',
-//         });
-//     }
-//
-//     if (Device.isDevice) {
-//         const {status: existingStatus} = await Notifications.getPermissionsAsync();
-//         let finalStatus = existingStatus;
-//         if (existingStatus !== 'granted') {
-//             const {status} = await Notifications.requestPermissionsAsync();
-//             finalStatus = status;
-//         }
-//         if (finalStatus !== 'granted') {
-//             handleRegistrationError('Permission not granted to get push token for push notification!');
-//             return;
-//         }
-//         const projectId =
-//             Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
-//         console.log('Project ID:', projectId); //todo remove
-//         if (!projectId) {
-//             handleRegistrationError('Project ID not found');
-//         }
-//         try {
-//             const pushTokenString = (
-//                 await Notifications.getExpoPushTokenAsync({
-//                     projectId,
-//                 })
-//             ).data;
-//             console.log(pushTokenString);
-//             return pushTokenString;
-//         } catch (e: unknown) {
-//             handleRegistrationError(`${e}`);
-//         }
-//     } else {
-//         handleRegistrationError('Must use physical device for push notifications');
-//     }
-// }
 
 export default function App() {
     const [isReady, setIsReady] = useState(false);
@@ -130,26 +82,6 @@ export default function App() {
     );
     const notificationListener = useRef<Notifications.EventSubscription>();
     const responseListener = useRef<Notifications.EventSubscription>();
-    // useEffect(() => {
-    //     registerForPushNotificationsAsync()
-    //         .then(token => setExpoPushToken(token ?? ''))
-    //         .catch((error: any) => setExpoPushToken(`${error}`));
-    //
-    //     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
-    //         setNotification(notification);
-    //     });
-    //
-    //     responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-    //         console.log(response);
-    //     });
-    //
-    //     return () => {
-    //         notificationListener.current &&
-    //         Notifications.removeNotificationSubscription(notificationListener.current);
-    //         responseListener.current &&
-    //         Notifications.removeNotificationSubscription(responseListener.current);
-    //     };
-    // }, []);
     useEffect(() => {
         const notificationListener = Notifications.addNotificationReceivedListener((notification) => {
             setNotification(notification);
